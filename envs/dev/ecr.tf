e2a59dd3b76ed5f3ce1b2f90f2ec85c318dacf13
Yu Jin <qldls0307@naver.com>
2026-05-30 19:53:12 +0900

    fix: allow deleting non-empty ECR repositories

--- envs/dev/ecr.tf
+++ envs/dev/ecr.tf
@@ -3,6 +3,7 @@ resource "aws_ecr_repository" "service" {
 
   name                 = "${local.name_prefix}-${each.value.module}"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
